d21d418e9d9b22156f964354c156ad4d2e377c16
India <India@India-PC>
2024-01-20 22:58:01 +0530

    m

---
+++ main.tf
@@ -0,0 +1,111 @@
+	resource "aws_security_group" "main" {
+	  name        = "${var.env}-${var.component}-sg"
+	  description = "${var.env}-${var.component}-sg"
+	  vpc_id      = var.vpc_id
+	
+	  ingress {
+	    description = "ELASTICACHE"
+	    from_port   = 5672
+	    to_port     = 5672
+	    protocol    = "tcp"
+	    cidr_blocks = [var.vpc_cidr]
+	  }
+	
+	  ingress {
+	    description = "SSH"
+	    from_port   = 22
+	    to_port     = 22
+	    protocol    = "tcp"
+	    cidr_blocks = var.bastion_node_cidr
+	  }
+	
+	  egress {
+	    from_port   = 0
+	    to_port     = 0
+	    protocol    = "-1"
+	    cidr_blocks = ["0.0.0.0/0"]
+	  }
+	
+	  tags = {
+	    Name = "${var.env}-${var.component}-sg"
+	  }
+	}
+	
+	resource "aws_iam_role" "role" {
+	  name = "${var.env}-${var.component}-role"
+	
+	  assume_role_policy = jsonencode({
+	    Version = "2012-10-17"
+	    Statement = [
+	      {
+	        Action = "sts:AssumeRole"
+	        Effect = "Allow"
+	        Sid    = ""
+	        Principal = {
+	          Service = "ec2.amazonaws.com"
+	        }
+	      },
+	    ]
+	  })
+	
+	  inline_policy {
+	    name = "${var.env}-${var.component}-policy"
+	
+	    policy = jsonencode({
+	      "Version" : "2012-10-17",
+	      "Statement" : [
+	        {
+	          "Sid" : "VisualEditor0",
+	          "Effect" : "Allow",
+	          "Action" : [
+	            "kms:Decrypt",
+	            "ssm:DescribeParameters",
+	            "ssm:GetParameterHistory",
+	            "ssm:GetParametersByPath",
+	            "ssm:GetParameters",
+	            "ssm:GetParameter"
+	          ],
+	          "Resource" : "*"
+	        }
+	      ]
+	    })
+	  }
+	
+	  tags = {
+	    tag-key = "${var.env}-${var.component}-role"
+	  }
+	}
+	
+	resource "aws_iam_instance_profile" "instance_profile" {
+	  name = "${var.env}-${var.component}-role"
+	  role = aws_iam_role.role.name
+	}
+	
+	resource "aws_instance" "main" {
+	  ami                    = data.aws_ami.ami.id
+	  vpc_security_group_ids = [aws_security_group.main.id]
+	  instance_type          = var.rabbitmq_instance_type
+	  subnet_id              = var.subnets[0]
+	  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+	
+	  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+	    role_name = var.component,
+	    env       = var.env
+	  }))
+	
+	  tags = {
+	    Name = "${var.env}-${var.component}"
+	  }
+	
+	  root_block_device {
+	    encrypted  = true
+	  }
+	}
+	
+	resource "aws_route53_record" "record" {
+	  zone_id = var.zone_id
+	  name    = "${var.component}-${var.env}"
+	  type    = "A"
+	  ttl     = 30
+	  records = [aws_instance.main.private_ip]
+	}
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "subnets" {}
+variable "rabbitmq_instance_type" {}
+variable "zone_id" {}
+variable "bastion_node_cidr" {}
